a994622aac3d6cfbb1248026aafdc4ccd971c032
Sung Paik <sung.paik@codeimmersives.com>
2019-10-09 14:12:42 -0400

    completed first three task

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+function appendToUl(element) {
+    const ul = document.querySelector('ul');
+    ul.appendChild(element);
+}
+
+function appendToDiv(element) {
+    const lorem = document.querySelector('div#lorem');
+    lorem.appendChild(element);
+}
+
+function addText(str) {
+    const li = document.querySelector('li');
+    li.innerText = str;
+}
 
